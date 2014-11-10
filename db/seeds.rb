dbad3ac6cbecbcea393339febe80019e85d2ae51
daynaclowe <daynalowe@Dayna-Lowes-MacBook.local>
2014-11-10 18:08:54 -0500

    Displaying price in dollars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: "My Product", description: "A great product", :price_in_cents "100")
